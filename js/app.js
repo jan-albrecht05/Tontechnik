c3fe7810735eacdf9f333a85aecb7f0b0a1884d7
Jan <janalbrecht05@gmail.com>
2022-09-16 17:00:25 +0200

    adding the light/dark toggle switch

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var white = "#ffffff";
+var black = "#262626";
+var block = "block";
+var none = "none";
+var darkimg = "url(/img/Mixer.jpg)";
+var lightimg = "url(/img/Mixer3.jpg)";
+function changetodark(){
+    document.getElementById("body").style.backgroundColor = black;
+    document.getElementById("body").style.color = white;
+    document.getElementById("body").style.backgroundImage = darkimg;
+    document.getElementById("content").style.color = white;
+    document.getElementById("forlight").style.display = block;
+    document.getElementById("fordark").style.display = none;
+}
+function changetolight(){
+    document.getElementById("body").style.backgroundColor = white;
+    document.getElementById("body").style.color = black;
+    document.getElementById("body").style.backgroundImage = lightimg;
+    document.getElementById("content").style.color = black;
+    document.getElementById("forlight").style.display = none;
+    document.getElementById("fordark").style.display = block;
+}
