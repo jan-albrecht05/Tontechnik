e381f6054efe8b5cae01f2c83037c58b09c6e420
Jan <janalbrecht05@gmail.com>
2022-09-19 21:41:24 +0200

    adding content to kontakt und veranstaltungen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var white = "#ffffff";
-var black = "#262626";
+var dark = "#262626";
 var block = "block";
 var none = "none";
 var darkimg = "url(/img/Mixer.jpg)";
 var lightimg = "url(/img/Mixer3.jpg)";
 function changetodark(){
-    document.getElementById("body").style.backgroundColor = black;
+    document.getElementById("body").style.backgroundColor = dark;
     document.getElementById("body").style.color = white;
     document.getElementById("body").style.backgroundImage = darkimg;
     document.getElementById("content").style.color = white;
@@ -15,9 +15,9 @@ function changetodark(){
 }
 function changetolight(){
     document.getElementById("body").style.backgroundColor = white;
-    document.getElementById("body").style.color = black;
+    document.getElementById("body").style.color = dark;
     document.getElementById("body").style.backgroundImage = lightimg;
-    document.getElementById("content").style.color = black;
+    document.getElementById("content").style.color = dark;
     document.getElementById("forlight").style.display = none;
     document.getElementById("fordark").style.display = block;
     //document.getElementById("navbar").querySelector('.reiter').style.color = white;
