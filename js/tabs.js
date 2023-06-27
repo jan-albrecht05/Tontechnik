79018bfa3e0a5f64419d34c1d4a3afeac8c4957b
Jan <janalbrecht05@gmail.com>
2023-06-27 15:18:53 +0200

    adding a script for the switching between the tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,20 @@
-function opentab1(){
-    document.getElementById("tab1").style.display = "block";
-    document.getElementById("tab2").style.display = "none";
-    document.getElementById("for1").style.backgroundColor = "#323232";
-    document.getElementById("for1").style.textDecoration = "underline";
-    document.getElementById("for2").style.textDecoration = "none";
-    document.getElementById("for2").style.backgroundColor = "#545454";
+function checkURL(){
+    let page = window.location.search;
+    if(page == "?stunden"){
+        document.getElementById("tab1").style.display = "block";
+        document.getElementById("tab2").style.display = "none";
+        document.getElementById("for2").style.backgroundColor = "#323232";
+        document.getElementById("for1").style.backgroundColor = "var(--m-tabs)";
+    }else{
+        document.getElementById("tab1").style.display = "none";
+        document.getElementById("tab2").style.display = "block";
+        document.getElementById("for1").style.backgroundColor = "#323232";
+        document.getElementById("for2").style.backgroundColor = "var(--m-tabs)";
+    }
 }
-
+function opentab1(){
+    location = window.location.pathname+"?stunden";
+}
 function opentab2(){
-    document.getElementById("tab1").style.display = "none";
-    document.getElementById("tab2").style.display = "block";
-    document.getElementById("for2").style.backgroundColor = "#323232";
-    document.getElementById("for2").style.textDecoration = "underline";
-    document.getElementById("for1").style.textDecoration = "none";
-    document.getElementById("for1").style.backgroundColor = "#545454";
+    location = window.location.pathname+"?events";
 }
