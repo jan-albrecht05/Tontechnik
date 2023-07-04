c945782bdaf733bfd388c60cc9ff51896d5810cf
Jan <janalbrecht05@gmail.com>
2023-07-04 18:26:06 +0200

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,15 @@
 function checkURL(){
     let page = window.location.search;
-    if(page == "?stunden"){
-        document.getElementById("tab1").style.display = "block";
-        document.getElementById("tab2").style.display = "none";
-        document.getElementById("for2").style.backgroundColor = "#323232";
-        document.getElementById("for1").style.backgroundColor = "var(--m-tabs)";
-    }else{
+    if(page == "?events"){
         document.getElementById("tab1").style.display = "none";
         document.getElementById("tab2").style.display = "block";
         document.getElementById("for1").style.backgroundColor = "#323232";
         document.getElementById("for2").style.backgroundColor = "var(--m-tabs)";
+    }else{
+        document.getElementById("tab1").style.display = "block";
+        document.getElementById("tab2").style.display = "none";
+        document.getElementById("for2").style.backgroundColor = "#323232";
+        document.getElementById("for1").style.backgroundColor = "var(--m-tabs)";
     }
 }
 function opentab1(){
